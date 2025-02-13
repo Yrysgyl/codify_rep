0499d4a4a5f1f4693627537cb600482a5b566d58
Yrysgyl <164658092+Yrysgyl@users.noreply.github.com>
2025-02-13 22:36:00 +0600

    staticfile_for_git

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-=======
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
->>>>>>> 309f14f (second_commit)
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -24,13 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
-    path('app/', include('app.urls'))
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
     path('', include('movie.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> 309f14f (second_commit)
