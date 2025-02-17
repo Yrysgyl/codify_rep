96a3a14f6fbb081d12020f5b7705b5c5947a9593
Yrysgyl <164658092+Yrysgyl@users.noreply.github.com>
2025-02-17 22:19:28 +0600

    add create car function

--- car_app/urls.py
+++ car_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from . import views
 
 urlpatterns = [
-    path('', index, name='home'),  
+    path('', views.index, name='home'),  
+    path('car_create/', views.car_create_view, name='car_create'),
 ]
--- car_app/views.py
+++ car_app/views.py
@@ -1,4 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Category, Color, Car
 
 def index(request):
-    return render(request, 'index.html') 
+    return render(request, 'car_app/index.html') 
+
+def car_create_view(request):
+    categories = Category.objects.all()
+    colors = Color.objects.all()
+
+    if request.method == 'POST':
+        title = request.POST['title']
+        model = request.POST['model']
+        year = request.POST['year']
+        odometer = request.POST['odometer']
+        engine_capacity = request.POST['engine_capacity']
+        color_id = request.POST['color_id']
+        category_id = request.POST['category_id']
+        image = request.FILES['image']
+
+        category = Category.objects.get(id=category_id)
+        color = Color.objects.get(id=color_id)
+
+        car = Car(title=title, category=category, model=model, year=year, odometer=odometer,
+                  engine_capacity=engine_capacity, color=color, image=image)
+        car.save()
+
+        return redirect('home')
+
+    return render(request, 'car_app/car_create.html', context={"categories": categories, "colors": colors})
